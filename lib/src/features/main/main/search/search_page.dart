5ae00c5ef70caca34322735a115bd55f1a198de0
ofokov <abd.ofokov@gmail.com>
2023-08-27 10:55:55 +0500

    added search page = grid

--- lib/src/features/main/main/search/search_page.dart
+++ lib/src/features/main/main/search/search_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:project_77_uz/src/common/constants/app_color.dart';
 import 'package:project_77_uz/src/features/main/main/search/widgets_for_search/good_categories.dart';
+import 'package:project_77_uz/src/features/widgets/grid_product.dart';
 
 import '../../../../common/constants/app_icons.dart';
 import '../../../widgets/custom_appbar.dart';
@@ -12,7 +13,8 @@ class SearchPage extends StatefulWidget {
   State<SearchPage> createState() => _SearchPageState();
 }
 
-class _SearchPageState extends State<SearchPage> with AutomaticKeepAliveClientMixin {
+class _SearchPageState extends State<SearchPage>
+    with AutomaticKeepAliveClientMixin {
   @override
   Widget build(BuildContext context) {
     super.build(context);
@@ -27,46 +29,67 @@ class _SearchPageState extends State<SearchPage> with AutomaticKeepAliveClientMi
         backgroundColor: Colors.white,
       ),
       backgroundColor: AppColor.white,
-      body: ListView(
-        children: [
-          Padding(
-            padding: EdgeInsets.all(screenHeight * 0.025),
-            child: SizedBox(
-              height: screenHeight * 0.08,
-              child: TextField(
-                decoration: InputDecoration(
-                  prefixIcon: const Icon(Icons.search),
-                  border: const OutlineInputBorder(),
-                  hintText: 'Что вы ищите?',
-                  suffix: ElevatedButton(
-                    onPressed: () {},
-                    child: const Text("Поиск"),
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: Colors.black,
-                      foregroundColor: Colors.white,
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(5),
+      body: NestedScrollView(
+        headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
+          return [
+            SliverAppBar(
+              expandedHeight: screenHeight*0.8,
+              backgroundColor: AppColor.white,
+              toolbarHeight: 0,
+              flexibleSpace: FlexibleSpaceBar(
+                background: Column(
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.all(screenHeight * 0.025),
+                      child: SizedBox(
+                        height: screenHeight * 0.08,
+                        child: TextField(
+                          decoration: InputDecoration(
+                            prefixIcon: const Icon(Icons.search),
+                            border: const OutlineInputBorder(),
+                            hintText: 'Что вы ищите?',
+                            suffix: ElevatedButton(
+                              onPressed: () {},
+                              child: const Text("Поиск"),
+                              style: ElevatedButton.styleFrom(
+                                backgroundColor: Colors.black,
+                                foregroundColor: Colors.white,
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(5),
+                                ),
+                              ),
+                            ),
+                            focusColor: Colors.black,
+                          ),
+                        ),
                       ),
                     ),
-                  ),
-                  focusColor: Colors.black,
+                    Padding(
+                      padding:
+                          EdgeInsets.symmetric(horizontal: screenHeight * 0.03),
+                      child: const Text("Популярные поиски"),
+                    ),
+                    for (int i = 0; i < categories.length; i++)
+                      Column(
+                        children: [
+                          GoodCategories(
+                            categoryName: categories[i],
+                          ),
+                        ],
+                      ),
+                  ],
                 ),
+                collapseMode: CollapseMode.pin,
               ),
             ),
+          ];
+        },
+        body: const Expanded(
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 15.0, vertical: 5),
+            child: GridProduct(),
           ),
-          Padding(
-            padding: EdgeInsets.symmetric(horizontal: screenHeight * 0.03),
-            child: const Text("Популярные поиски"),
-          ),
-          for (int i = 0; i < categories.length; i++)
-            Column(
-              children: [
-                GoodCategories(
-                  categoryName: categories[i],
-                ),
-              ],
-            ),
-        ],
+        ),
       ),
     );
   }
